3ddcf6d64bb6070e45c4c59760f1db9baf28af79
Muhammad Rifki <mrifki028@gmail.com>
2023-03-10 11:14:08 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,37 @@
+const {
+  getAllBooksHandler,
+  addBookHandler,
+  getBookByIdHandler,
+  updateBookByIdHandler,
+  deleteBookHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getBookByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: updateBookByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: deleteBookHandler,
+  },
+];
+
+module.exports = routes;
